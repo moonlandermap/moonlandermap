2e869e0585e98802a7ba6610754681eb7145d956
boilermaker021 <nathan.leakeas@gmail.com>
2024-11-19 16:00:10 -0500

    new test

--- helloWorldWind.js
+++ helloWorldWind.js
@@ -12,7 +12,52 @@ moonLayer.addRenderable(moonTexture);
 // Add the Moon layer to the WorldWind instance
 wwd.addLayer(moonLayer);
 
+
+//add markers to moon
+var placemarkLayer = new WorldWind.RenderableLayer("Placemark");
+wwd.addLayer(placemarkLayer);
+
+var placemarkAttributes = new WorldWind.PlacemarkAttributes(null);
+placemarkAttributes.imageSource = "usa.png";
+placemarkAttributes.imageScale = 0.02;
+placemarkAttributes.labelAttributes.color = WorldWind.Color.WHITE;
+placemarkAttributes.labelAttributes.offset = new WorldWind.Offset(
+    WorldWind.OFFSET_FRACTION, 0.5,
+    WorldWind.OFFSET_FRACTION, 1.0
+);
+
+var position = new WorldWind.Position(-15.5, -130.7, 0); // Latitude, Longitude, Altitude
+var placemark = new WorldWind.Placemark(position, false, placemarkAttributes);
+placemark.label = "Ranger 4";
+placemark.alwaysOnTop = true;
+
+placemarkLayer.addRenderable(placemark);
+wwd.addLayer(placemarkLayer);
+
 // Add other useful layers (optional)
 wwd.addLayer(new WorldWind.CompassLayer());
 wwd.addLayer(new WorldWind.CoordinatesDisplayLayer(wwd));
 wwd.addLayer(new WorldWind.ViewControlsLayer(wwd));
+
+//stars
+// Add the star field layer
+var starFieldLayer = new WorldWind.StarFieldLayer();
+wwd.addLayer(starFieldLayer);
+
+
+wwd.addEventListener("click", function (event) {
+    var x = event.clientX;
+    var y = event.clientY;
+    var pickList = wwd.pick(wwd.canvasCoordinates(x, y));
+
+    if (pickList.objects.length > 0) {
+        pickList.objects.forEach(function (pickedObject) {
+            if (pickedObject.userObject instanceof WorldWind.Placemark) {
+                var clickedPlacemark = pickedObject.userObject;
+                alert("You clicked on: " + clickedPlacemark.label);
+                // Navigate to a link (optional)
+                // window.location.href = "https://example.com/landing-site";
+            }
+        });
+    }
+});
